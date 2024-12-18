701d8081433b00d9dbdb52da1999526f74cda717
Rozak-Al <asefiftyeight@gmail.com>
2024-12-18 13:32:31 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,208 @@
+
+const teams = [ 
+    {
+        "name": "Muhammad Khaidir Fahram",
+        "image": "https://fahram.com//img/profile-photos/jzZj0jPuEKTWwY9a8lxo7pQhA29zxFpVQd7xl4BZ.jpg",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/khaidir.fahram"
+            },
+            {
+                "name" : "facebook",
+                "link" : "https://facebook.com/khaidir.fahram"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fahram.com/"
+            }
+        ]
+    },
+    {
+        "name": "Achmad Maulana Ichsan",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://instagram.com/a.iichsan"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://icsan.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rozak Al Baidawi",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/_rzkalbdwi?igsh=MXNrM2Vvc2tkZWVi"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://rozak19.my.id/"
+            },
+            {
+                "name" : "facebook",
+                "link" : "https://www.facebook.com/share/FYLMJHJz3stNeYXh/?mibextid=LQQJ4"
+            },
+            {
+                "name" : "envelope",
+                "link" : "mailto:rozakalbaidawi@gmail.com"
+            },
+        ]
+    },
+
+    {
+        "name": "Naufal Laudza",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/naufalaudzaa"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://naufal19.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Fredo Pradana",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/fredopradana"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fredo.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Hanif Dwi Novario",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/hnfdwnvr"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://hanifdwi.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Fikri Firdaus",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/m_f1kr1"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fikriy.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Sandi Nourman",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/sndiinrmn"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://sandinourman.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Marcella Setyaning Putri",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/mqrcelllaaaa"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://marcellasetyaningputri.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Rheina Wardiana Hafiroh",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/na_rrrrr44"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://rheinawardianahafiroh.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Abdulrahman Arafah Sunarto",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/arafah.abdulrahman_"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://fahganindo4life.my.id/"
+            }
+        ]
+    },
+
+    {
+        "name": "Siti Aisyah",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/gesterpaya"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://kaisyah.my.id"
+            }
+        ]
+    },
+
+    {
+        "name": "Muhammad Rivanda Ramdhani",
+        "image": "https://fastly.picsum.photos/id/288/200/300.jpg?hmac=45WLionXnoogi0-njKuSNnVY5hnswMhf-CrxwzKTcrc",
+        "link" : [
+            {
+                "name" : "instagram",
+                "link" : "https://www.instagram.com/rivanda"
+            },
+            {
+                "name" : "globe",
+                "link" : "https://rivanda.my.id/"
+            }
+        ]
+    },
+]
